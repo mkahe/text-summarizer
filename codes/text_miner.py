890ce32dd0c5dcbde0b5c2943a0042541a3188ec
vahidmohsseni <vahidmohsseni@yahoo.com>
2022-10-22 11:55:33 +0000

    Add data

--- codes/text_miner.py
+++ codes/text_miner.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from bs4 import BeautifulSoup as bs 
 import requests
 
@@ -7,12 +8,19 @@ def get_text(url):
     html = requests.get(url).text
     # Parse the HTML
     soup = bs(html, 'lxml')
-    # Get the text
-    text = soup.get_text()
-    return text
+    # Get the H1 tag text
+    h1 = soup.find('h1').text
+    print(h1)
+    # Get the all paragraphs text after the h1 tag and before h2 tag
+    p = soup.find('h1').find_next_siblings('p')
+
+    # p = soup.find('h1').find_all_next('p')
+    print(p)
+
 
 if __name__ == "__main__":
     # Get the text
     text = get_text("https://en.wikipedia.org/wiki/Finland")
+    # text = get_text("https://en.wikipedia.org/wiki/Oulu")
     # Print the first 1000 characters
-    print(text[:1000])
+    # print(text[:1000])
